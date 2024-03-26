87c958ad31e259f492b94df5ebcf14c6376f23bb
Yves <yves.as@gmail.com>
2024-03-26 15:33:38 -0300

    model 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
+/* eslint-disable no-unused-vars */
 import './App.css'
-import Permissions from './components/Permissions'
+import { useState, useRef } from "react";
+// import Permissions from './components/Permissions'
 // import WebcamStreamCapture from './components/WebcamStreamCapture.jsx'
+import VideoRecorder from "./components/VideoRecorder";
+import AudioRecorder from "./components/AudioRecorder";
 
-function App() {
-  
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Permissions/>
-      {/* <WebcamStreamCapture/> */}
-    </div>
-  )
-}
+const App = () => {
+    let [recordOption, setRecordOption] = useState("video");
+    const toggleRecordOption = (type) => {
+        return () => {
+            setRecordOption(type);
+        };
+    };
+    return (
+        <div>
+            <h1>React Media Recorder</h1>
+            <div className="button-flex">
+                <button onClick={toggleRecordOption("video")}>
+                  Record Video
+                </button>
+                <button onClick={toggleRecordOption("audio")}>
+                  Record Audio
+                </button>
+            </div>
+            <div>
+                {recordOption === "video" ? <VideoRecorder /> : <AudioRecorder />}
+            </div>
+        </div>
+    );
+};
+export default App;
 
-export default App
+    // return (
+    //   <div>
+    //     <h1>Hello</h1>
+    //     <Permissions/>
+    //     {/* <WebcamStreamCapture/> */}
+    //   </div>
+    // )
